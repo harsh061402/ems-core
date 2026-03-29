70fe07a1c94cbb6a1c2986dcd6dd41cdeb0de0c2
harsh061402 <harshkumar0614jain@gmail.com>
2026-03-30 01:29:29 +0530

    refactor: add validation in LeaveDecisionRequestModel

--- src/main/java/com/harshkumar0614jain/ems/model/LeaveDecisionRequestModel.java
+++ src/main/java/com/harshkumar0614jain/ems/model/LeaveDecisionRequestModel.java
@@ -1,10 +1,12 @@
 package com.harshkumar0614jain.ems.model;
 
 import com.harshkumar0614jain.ems.enums.LeaveStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class LeaveDecisionRequestModel {
+    @NotNull(message = "Leave Status is required")
     private LeaveStatus leaveStatus;
     private String managerComment;
 }
