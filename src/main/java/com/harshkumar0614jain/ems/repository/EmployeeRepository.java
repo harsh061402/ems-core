103ae7867b78af338588591be6dfeed46e63c88e
harsh061402 <harshkumar0614jain@gmail.com>
2026-03-30 04:28:49 +0530

    feat(employee): add status-based filter query methods in EmployeeRepository

--- src/main/java/com/harshkumar0614jain/ems/repository/EmployeeRepository.java
+++ src/main/java/com/harshkumar0614jain/ems/repository/EmployeeRepository.java
@@ -1,12 +1,18 @@
 package com.harshkumar0614jain.ems.repository;
 
 import com.harshkumar0614jain.ems.entity.Employee;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import com.harshkumar0614jain.ems.enums.Department;
+import com.harshkumar0614jain.ems.enums.EmployeeStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
 
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
     boolean existsByMobileNumber(String mobileNumber);
 
     boolean existsByUserId (String userId);
+
+    List<Employee> findByDepartmentAndEmployeeStatusNot(Department department, EmployeeStatus status);
+
+    List<Employee> findByEmployeeStatusNot(EmployeeStatus status);
 }
